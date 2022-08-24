c4964fef7a8a33af693d2bc5a1864d26fbb1972a
Rafael Felipe <rfaeldel@outlook.com>
2022-08-24 18:12:04 -0300

    update app.js

--- Console.log/app.js
+++ Console.log/app.js
@@ -3,3 +3,6 @@ console.log("Hello World"); // String
 console.log(`'Hello' "World"`); // String
 
 console.log(123456, 35.12, "Hello World"); //Number String
+
+// Exercício
+console.log('Meu nome é "Rafael". Estou aprendendo JavaScript às '+17+' da tarde')
