f53162f812eb131f852ce5c95aec794d61f3d9aa
Rafael Felipe <rfaeldel@outlook.com>
2022-08-26 13:39:14 -0300

    atualização de conteúdos

--- Console.log/app.js
+++ Console.log/app.js
@@ -5,4 +5,4 @@ console.log(`'Hello' "World"`); // String
 console.log(123456, 35.12, "Hello World"); //Number String
 
 // Exercício
-console.log('Meu nome é "Rafael". Estou aprendendo JavaScript às '+17+' da tarde')
+console.log('Meu nome é "Rafael". Estou aprendendo JavaScript às ',17,' da tarde')
---
+++ Variáveis com Let/index.js
@@ -0,0 +1,7 @@
+let nome = 'Giovanni';
+
+console.log(nome,'nasceu em 1984');
+console.log('Em 2000,',nome,'conheceu Maria');
+console.log(nome,`casou-se em 2012 com Maria`);
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Edoardo');
